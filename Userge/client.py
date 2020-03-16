a77b4208d2549db4a47ca916a28597b9ba2b45e5
rking32 <sljohnwick@gmail.com>
2020-03-16 19:58:57 +0530

    fix logger shit :)

--- Userge/client.py
+++ Userge/client.py
@@ -1,8 +1,4 @@
 from .utils import Config, logging
-
-log = logging.getLogger(__name__)
-
-
 from pyrogram import Client, Filters
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
@@ -14,8 +10,8 @@ class Userge(Client):
     LOG = logging
 
     def __init__(self):
-        self.log = log
-        log.info("setting configs...")
+        self.log = logging.getLogger(__name__)
+        self.log.info("setting configs...")
         super().__init__(
             Config.HU_STRING_SESSION,
             api_id=Config.API_ID,
@@ -24,15 +20,15 @@ class Userge(Client):
         )
 
     def getLogger(self, name: str):
-        log.info(f"creating new logger => {name}")
+        self.log.info(f"creating new logger => {name}")
         return self.LOG.getLogger(name)
 
     def cmd(self, command: str):
-        log.info(f"setting new command => {command}")
+        self.log.info(f"setting new command => {command}")
         return Filters.command(command, ".") & Filters.me
 
     def add_help(self, command: str, about: str):
-        log.info(f"help dict updated {command} : {about}")
+        self.log.info(f"help dict updated {command} : {about}")
         self.HELP_DICT.update({command: about})
 
     def get_help(self, key: str = None) -> dict:
