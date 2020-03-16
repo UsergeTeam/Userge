56386b163a37150f1f3ba1e08ee1fe3658842a34
Udith <uaudith@gmail.com>
2020-03-16 19:08:09 +0530

    introducing some functions for db
    Signed-off-by: Udith <uaudith@gmail.com>

---
+++ Userge/db.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+from .utils import Config
+
+
+class Database:
+
+    def __init__(self, tablename):
+        client = MongoClient(Config.DB_URI)
+        self.db = client[tablename]
+
+    def addnew(self, dictionary):
+        result = self.db.reviews.insert_one(dictionary)
+        return result
+
+    def findone(self, key, value: object) -> object:
+        return self.db.reviews.find_one({key: value})
+
+    def update(self, existingentry, new_dictionary_entry, set_unset):
+        if set_unset is 'set':
+            x = '$set'
+        else:
+            x = '$unset'
+        result = self.db.reviews.update_one(existingentry, {x: new_dictionary_entry})
+        return result
+
+    def delall(self):
+        self.db.reviews.drop()
+
+    def filter(self, query: dict, output: dict):
+        return self.db.reviews.find(query, output)
+
+    def never_used(self, output: dict):
+        return self.filter({"times_used": {'exists': False}}, output)
