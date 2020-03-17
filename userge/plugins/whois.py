e3e387e50ad1d05c7127d89d069b8aa3c391bd1d
K-E-N-W-A-Y <25846872+K-E-N-W-A-Y@users.noreply.github.com>
2020-03-17 20:19:59 +0530

    added new whois options

--- userge/plugins/whois.py
+++ userge/plugins/whois.py
@@ -20,10 +20,18 @@ async def who_is(_, message):
         await message.edit("no valid user_id / message specified")
         return
     if from_user is not None:
-        message_out_str = ""
-        message_out_str += f"ID: `{from_user.id}`\n"
+        message_out_str += f"<strong>USER INFO:</strong>" \n
+        message_out_str += "" \n
         message_out_str += f"First Name: <a href='tg://user?id={from_user.id}'>{from_user.first_name}</a>\n"
-        message_out_str += f"Last Name: {from_user.last_name}"
+        message_out_str += f"Last Name: {from_user.last_name}\n"
+        message_out_str += f"Username: @{from_user.username}\n"
+        message_out_str += f"Data Centre ID: {from_user.dc_id}\n"
+        message_out_str += f"Is Bot: {from_user.is_bot}\n"
+        message_out_str += f"Is Restricted: {from_user.is_scam}\n"
+        message_out_str += f"Is Verified by Telegram: {from_user.is_verified}\n"
+        message_out_str += f"ID: <code>{from_user.id}</code>\n"
+        message_out_str += ""
+        message_out_str += f"Bio: {from_user.}"
         chat_photo = from_user.photo
         local_user_photo = await userge.download_media(
             message=chat_photo.big_file_id
