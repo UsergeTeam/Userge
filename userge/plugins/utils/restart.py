40e09a0ab2a1440206e654ed22d4ff3e73795ac1
Udith <uaudith@gmail.com>
2020-03-27 23:28:30 +0530

    fixed restart module
    Signed-off-by: Udith <uaudith@gmail.com>

--- userge/plugins/utils/restart.py
+++ userge/plugins/utils/restart.py
@@ -6,12 +6,14 @@ LOG = userge.getLogger(__name__)
 
 @userge.on_cmd('restart', about="__Restarts the bot and load all plugins__")
 async def restart_cmd_handler(m: Message):
-    await m.reply("Restarting Userge Services")
+    await m.edit("Restarting Userge Services")
     LOG.info(f"USERGE Services - Restart initiated")
     asyncio.create_task(restart(userge, m))
 
 
 async def restart(c: userge, m: Message):
-    await c.restart()
+    await c.stop()
+    await c.reload_plugins()
+    await c.start()
     await m.edit(f"USERGE Services have been Restarted!")
     LOG.info(f"USERGE - Restarted")
