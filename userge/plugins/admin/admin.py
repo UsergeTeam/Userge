cf5ccbe5aaa05376661d40a3f3d870280273073f
K-E-N-W-A-Y <25846872+K-E-N-W-A-Y@users.noreply.github.com>
2020-03-29 22:01:17 +0530

    fix promote func + added more bugs to fix

--- userge/plugins/admin/admin.py
+++ userge/plugins/admin/admin.py
@@ -27,102 +27,31 @@ async def promote_usr(message: Message):
 
     if promote_perm:
 
-        if " " in message.input_str:
-            user_id, user_rank = message.input_str.split(" ")
+        user_id = message.input_str
 
-            if user_id and user_rank is not None:
-                try:
-                    await userge.promote_chat_member(chat_id, user_id,
-                                                     can_change_info=True,
-                                                     can_post_messages=True,
-                                                     can_edit_messages=True,
-                                                     can_delete_messages=True,
-                                                     can_restrict_members=True,
-                                                     can_invite_users=True,
-                                                     can_pin_messages=True,
-                                                     can_promote_members=False,)
+        if user_id:
+            try:
+                await userge.promote_chat_member(chat_id, user_id)
 
-                    await userge.set_administrator_title(chat_id, user_id, user_rank)
+                await message.edit("**👑 Promoted Successfully**", del_in=5)
 
-                    await message.edit("**👑 Promoted Successfully**")
-
-                except Exception as e:
-                    await message.edit(
-                        text=f"`something went wrong 🤔, do .help promote for more info` \n **ERROR** {str(e)}")
-
-            else:
-                try:
-                    user_id = message.input_str
-
-                    await userge.promote_chat_member(chat_id, user_id,
-                                                     can_change_info=True,
-                                                     can_post_messages=True,
-                                                     can_edit_messages=True,
-                                                     can_delete_messages=True,
-                                                     can_restrict_members=True,
-                                                     can_invite_users=True,
-                                                     can_pin_messages=True,
-                                                     can_promote_members=False,)
-
-                    await message.edit("**👑 Promoted Successfully**")
-
-                except Exception as e:
-                    await message.edit(
-                        text=f"`something went wrong 🤔, do .help promote for more info` \n **ERROR** {str(e)}")
-
-                return
-
-        elif message.reply_to_message:
-            user_id = message.reply_to_message.from_user.id
-            user_rank = message.input_str
-
-            if user_rank is not None:
-
-                try:
-                    await userge.promote_chat_member(chat_id, user_id)
-
-                    await userge.set_administrator_title(chat_id, user_id, user_rank,
-                                                     can_change_info=True,
-                                                     can_post_messages=True,
-                                                     can_edit_messages=True,
-                                                     can_delete_messages=True,
-                                                     can_restrict_members=True,
-                                                     can_invite_users=True,
-                                                     can_pin_messages=True,
-                                                     can_promote_members=False,)
-
-                    await message.edit("**👑 Promoted Successfully**")
-
-                except Exception as e:
-                    await message.edit(
-                        text=f"`something went wrong 🤔, do .help promote for more info` \n **ERROR** {str(e)}")
-
-            else:
-
-                try:
-                    await userge.promote_chat_member(chat_id, user_id,
-                                                     can_change_info=True,
-                                                     can_post_messages=True,
-                                                     can_edit_messages=True,
-                                                     can_delete_messages=True,
-                                                     can_restrict_members=True,
-                                                     can_invite_users=True,
-                                                     can_pin_messages=True,
-                                                     can_promote_members=False,)
-
-                    await message.edit("**👑 Promoted Successfully**")
-
-                except Exception as e:
-                    await message.edit(
-                        text=f"`something went wrong 🤔, do .help promote for more info`\n **ERROR** {str(e)}")
-
-                return
+            except Exception as e:
+                await message.edit(
+                    text=f"`something went wrong 🤔, do .help promote for more info` \n **ERROR** {str(e)}")
 
         else:
-            await message.edit(
-                text="`no valid user_id or message specified, do .help promote for more info`")
+
+            user_id = message.reply_to_message.from_user.id
+
+            try:
+                await userge.promote_chat_member(chat_id, user_id)
+
+                await message.edit("**👑 Promoted Successfully**", del_in=5)
+
+            except Exception as e:
+                await message.edit(
+                    text=f"`something went wrong 🤔, do .help promote for more info` \n **ERROR** {str(e)}")
 
     else:
         await message.edit(
             text="`I don't have proper admin permission to do that ⚠`")
-    return
