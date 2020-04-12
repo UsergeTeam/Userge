ccc10c4b2463cd604405c2b592efb4aea80fdb60
rking32 <sljohnwick@gmail.com>
2020-04-12 13:20:49 +0530

    fixed some shits ;)

--- genStrSession.py
+++ genStrSession.py
@@ -1,12 +1,10 @@
 # Copyright (C) 2020 by UsergeTeam@Telegram, < https://t.me/theUserge >.
 #
-# All rights reserved.
-#
 # This file is part of < https://github.com/uaudith/Userge > project,
 # and is released under the "GNU v3.0 License Agreement".
 # Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
 #
-# file that should have been included as part of this package.
+# All rights reserved.
 
 
 from pyrogram import Client
