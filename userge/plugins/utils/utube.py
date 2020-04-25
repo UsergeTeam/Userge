25e1bd20b6cd4fd7fb96b446a754306d95e40645
Udith <uaudith@gmail.com>
2020-04-25 15:49:01 +0530

    minor fixings on ytdl and flags for video formats improved help
    Signed-off-by: Udith <uaudith@gmail.com>

--- userge/plugins/utils/utube.py
+++ userge/plugins/utils/utube.py
@@ -11,6 +11,7 @@ import youtube_dl as ytdl
 from os import path
 import asyncio
 from time import time
+
 from userge.utils import time_formatter, humanbytes
 
 
@@ -21,9 +22,9 @@ def yt_getInfo(link):
         formats = x.get('formats', [x])
         out = "No formats found :("
         if formats:
-            out = "--U-ID   |   Resolution   |   Extension--\n"
+            out = "--U-ID   |   Reso.  |   Extension--\n"
         for i in formats:
-            out += f"`{i['format_id']} | {i.get('format_note', None)} | {i.get('ext', None)} `\n"
+            out += f"`{i['format_id']} | {i.get('format_note', None)} | {i.get('ext', None)}`\n"
     except ytdl.utils.YoutubeDLError as e:
         return e
     else:
@@ -40,8 +41,8 @@ def supported(url):
 
 
 def tubeDl(url: list, prog, uid=None):
-    _opts = {'outtmpl': path.join('downloads', '%(extractor)s-%(title)s-%(format)s.%(ext)s')}
-    _quality = {'format': 'bestvideo+bestaudio/best' if not uid else str(uid)}
+    _opts = {'outtmpl': path.join('downloads', '%(title)s-%(format)s.%(ext)s')}
+    _quality = {'format': 'best' if not uid else str(uid)}
     _opts.update(_quality)
     try:
         x = ytdl.YoutubeDL(_opts)
@@ -53,7 +54,10 @@ def tubeDl(url: list, prog, uid=None):
         return dloader
 
 
-@userge.on_cmd("ytinfo", about={'header': "Get info from ytdl"})
+@userge.on_cmd("ytinfo", about={'header': "Get info from ytdl",
+                                'description': 'Get information of the link without downloading',
+                                'examples': '`.ytinfo link`',
+                                'others': 'To get info about direct links, use `.head link`'})
 async def ytinfo(message: Message):
     await message.edit("Hold on \u23f3 ..")
     _exracted = yt_getInfo(message.input_or_reply_str)
@@ -76,27 +80,38 @@ __{uploader}__
         await message.edit(out)
 
 
-@userge.on_cmd("ytdl", about={'header': "Download from youtube"})
+@userge.on_cmd("ytdl", about={'header': "Download from youtube",
+                              'options': {'-a': 'select the audio u-id',
+                                          '-v': 'select the video u-id'},
+                              'examples': ['.ytdl `link`',
+                                           '`.ytdl -a12 -v120 link`']}, del_pre=True)
 async def ytDown(message: Message):
     await message.edit("Hold on \u23f3 ..")
     desiredFormat = None
     startTime = time()
-    if bool(message.flags) & len(message.flags) <= 2:
-        desiredFormat = ''
+    if bool(message.flags):
+        desiredFormat1 = str(message.flags.get('a', ''))
+        desiredFormat2 = str(message.flags.get('v', ''))
+        if len(message.flags) == 2:
+            # 1st format must contain the video
+            desiredFormat = '+'.join([desiredFormat2, desiredFormat1])
+        elif len(message.flags) == 1:
+            desiredFormat = desiredFormat2 or desiredFormat1
 
     def __progress(data: dict):
-        if ((time() - startTime) % 3) > 2.9:
+        if ((time() - startTime) % 4) > 3.9:
             if data['status'] == "downloading":
                 eta = data['eta']
                 speed = data['speed']
                 if not (eta and speed):
                     return
                 out = "**Speed** >> {}/s\n**ETA** >> {}\n".format(humanbytes(speed), time_formatter(eta))
+                out += f'**File Name** >> __{data["filename"]}__\n\n'
                 current = data['downloaded_bytes']
                 total = data["total_bytes"]
                 if current and total:
                     percentage = int(current) * 100 / int(total)
-                    out += f"Progress >> {int(percentage)}%\n\n"
+                    out += f"Progress >> {int(percentage)}%\n"
                     out += "[{}{}]".format(''.join(["█" for _ in range(floor(percentage / 5))]),
                                            ''.join(["░" for _ in range(20 - floor(percentage / 5))]))
                 if message.text != out:
