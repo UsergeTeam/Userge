086c2c1492937e7bf853a78ee0578e0536b8ffec
Phyco-Ninja <60768764+Phyco-Ninja@users.noreply.github.com>
2020-06-04 22:28:24 +0530

    Okay!

--- userge/plugins/admin/gban.py
+++ userge/plugins/admin/gban.py
@@ -328,23 +328,23 @@ async def gban_at_entry(message: Message):
             if c['user_id'] == user_id:
                 reason = c['reason']
                 try:
-                    if await guadmin_check(chat_id, user_id):
-                        await userge.kick_chat_member(chat_id, user_id)
-                        await message.reply(
-                            r"\\**#Userge_Antispam**//"
-                            "\n\n\nGlobally Banned User Detected in this Chat.\n\n"
-                            f"**User:** [{firstname}](tg://user?id={user_id})\n"
-                            f"**ID:** `{user_id}`\n**Reason:** `{reason}`\n\n"
-                            "**Quick Action:** Banned.")
-                        await GBAN_LOG.log(
-                            r"\\**#Antispam_Log**//"
-                            "\n\n**GBanned User $SPOTTED**\n"
-                            f"**User:** [{firstname}](tg://user?id={user_id})\n"
-                            f"**ID:** `{user_id}`\n**Reason:** {reason}\n**Quick Action:** "
-                            "Banned in {message.chat.title}")
-                except Exception:
+                    await userge.kick_chat_member(chat_id, user_id)
+                    await message.reply(
+                        r"\\**#Userge_Antispam**//"
+                        "\n\n\nGlobally Banned User Detected in this Chat.\n\n"
+                        f"**User:** [{firstname}](tg://user?id={user_id})\n"
+                        f"**ID:** `{user_id}`\n**Reason:** `{reason}`\n\n"
+                        "**Quick Action:** Banned.")
+                    await GBAN_LOG.log(
+                        r"\\**#Antispam_Log**//"
+                        "\n\n**GBanned User $SPOTTED**\n"
+                        f"**User:** [{firstname}](tg://user?id={user_id})\n"
+                        f"**ID:** `{user_id}`\n**Reason:** {reason}\n**Quick Action:** "
+                        "Banned in {message.chat.title}")
+                except ChatAdminRequired:
                     break
-    except Exception:
+    except Exception as e:
+        LOG.exception(e)
         pass
 
     if Config.ANTISPAM_SENTRY:
@@ -352,7 +352,7 @@ async def gban_at_entry(message: Message):
             if Config.SPAM_WATCH_API is not None:
                 SENTRY = spamwatch.Client(Config.SPAM_WATCH_API)
                 intruder = SENTRY.get_ban(user_id)
-                if intruder and await guadmin_check(chat_id, user_id):
+                if intruder:
                     await userge.kick_chat_member(chat_id, user_id)
                     await message.reply(
                         r"\\**#Userge_Antispam**//"
@@ -368,13 +368,13 @@ async def gban_at_entry(message: Message):
                         f"\n**User:** [{firstname}](tg://user?id={user_id})\n"
                         f"**ID:** `{user_id}`\n**Reason:** `{intruder.reason}`\n"
                         "**Quick Action:** Banned in {message.chat.title}\n\n$AUTOBAN #id{user_id}")
-        except Exception:
+        except ChatAdminRequired:
             pass
 
         try:
             res = requests.get(f'https://combot.org/api/cas/check?user_id={user_id}')
             res_dict = json.loads(res.text)
-            if res_dict['ok'] and await guadmin_check(chat_id, user_id):
+            if res_dict['ok']:
                 try:
                     reason = res_dict['result']['offenses']
                     await userge.kick_chat_member(chat_id, user_id)
@@ -392,8 +392,9 @@ async def gban_at_entry(message: Message):
                         f"\n**User:** [{firstname}](tg://user?id={user_id})\n"
                         f"**ID:** `{user_id}`\n**Reason:** `{reason}`\n**Quick Action:**"
                         " Banned in {message.chat.title}\n\n$AUTOBAN #id{user_id}")
-                except Exception:
+                except ChatAdminRequired:
                     pass
-        except Exception:
+        except Exception as e:
+            LOG.exception(e)
             pass
     message.continue_propagation()
