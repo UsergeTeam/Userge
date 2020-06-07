f81f67c262a7e7445cf9b9e2d9a2e1431507d497
unknown <byazade@yandex.com>
2020-06-07 14:08:04 +0300

    edit

--- userge/plugins/utils/log_pms.py
+++ userge/plugins/utils/log_pms.py
@@ -2,9 +2,9 @@ import asyncio
 import os
 import sys
 import time
-from random import choice, randint
 
-from userge import Config, Filters, Message, get_collection, userge
+
+from userge import Config, Filters, Message, userge
 from userge.utils import time_formatter
 
 PM_LOGGR_BOT_API_ID = int(-385618434)
@@ -14,15 +14,15 @@ PM_LOGGER = True
 @userge.on_filters(Filters.private & Filters.outgoing)
 async def outgoing_auto_approve(message: Message):
     check_bot = message.from_user.is_bot
-    chat_id = message.from_user.id
+    chat_id = message.chat_id
     my_id = await userge.get_me()
     my_id = my_id.id
     if not check_bot and PM_LOGGER:
         try:
             await userge.forward_messages(
-                chat_id=chat_id,
-                from_chat_id=PM_LOGGR_BOT_API_ID,
-                message_ids=message.message_id
+                PM_LOGGR_BOT_API_ID,
+                chat_id,
+                message.message_id
             )
         except Exception as err:
             exc_type, exc_obj, exc_tb = sys.exc_info()
