fd181d6c3fa5f4e54081bcb8a23b61c9b5e38610
Neal Gosalia <nealgosalia@gmail.com>
2020-07-26 00:02:04 +0530

    Update kang.py

--- userge/plugins/fun/kang.py
+++ userge/plugins/fun/kang.py
@@ -9,7 +9,6 @@
 # All rights reserved.
 
 import os
-import math
 import random
 import urllib.request
 
@@ -209,21 +208,10 @@ def resize_photo(photo: str) -> str:
     """ Resize the given photo to 512x512 """
     image = Image.open(photo)
     maxsize = (512, 512)
-    if (image.width and image.height) < 512:
-        size1 = image.width
-        size2 = image.height
-        if image.width > image.height:
-            scale = 512 / size1
-            size1new = 512
-            size2new = size2 * scale
-        else:
-            scale = 512 / size2
-            size1new = size1 * scale
-            size2new = 512
-        size1new = math.floor(size1new)
-        size2new = math.floor(size2new)
-        sizenew = (size1new, size2new)
-        image = image.resize(sizenew)
+    if max(image.width, image.height) < 512:
+        scale = 512 / max(image.width, image.height)
+        new_size = (int(image.width * scale), int(image.height * scale))
+        image = image.resize(new_size, Image.ANTIALIAS)
     else:
         image.thumbnail(maxsize)
     os.remove(photo)
