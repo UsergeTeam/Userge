625625c93ccc85af59bba99aa8cde16d24730693
UdaySriHarsha <46833234+UdaySriHarsha@users.noreply.github.com>
2020-08-08 14:44:03 +0530

    Update __init__.py

--- userge/__init__.py
+++ userge/__init__.py
@@ -9,6 +9,7 @@
 # All rights reserved.
 
 from userge.logger import logging  # noqa
+from userge.functions import deEmojify, media_to_image
 from userge.config import Config, get_version  # noqa
 from userge.core import (  # noqa
     Userge, Filters, Message, get_collection, pool)
