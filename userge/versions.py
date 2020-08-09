542511052afb1cbca22a0a9df2a893295bb06a0a
UdaySriHarsha <46833234+UdaySriHarsha@users.noreply.github.com>
2020-08-09 05:40:39 +0000

    Update versions.py

--- userge/versions.py
+++ userge/versions.py
@@ -1,10 +1,10 @@
 # pylint: disable=missing-module-docstring
 #
-# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020 by UdaySriHarsha@Github, < https://github.com/UdaySriharsha>.
 #
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# This file is part of < https://github.com/UdaySriHarsha/Userge > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+# Please see < https://github.com/UdaySriHarsha/Userge/blob/master/LICENSE >
 #
 # All rights reserved.
 
@@ -19,6 +19,6 @@ __micro__ = 7
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 
 __license__ = ("[GNU GPL v3.0]"
-               "(https://github.com/UsergeTeam/Userge/blob/master/LICENSE)")
+               "(https://github.com/UdaySriHarsha/Userge/blob/master/LICENSE)")
 
-__copyright__ = "[UsergeTeam](https://github.com/UsergeTeam)"
+__copyright__ = "[UdaySriHarsha](https://github.com/UdaySriharsha)"
