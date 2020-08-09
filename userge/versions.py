036e6f8c284553220dd4faf23f776ccfe08d1cf6
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-09 13:19:16 +0530

    extends userge.utils (#122)
    * let me add short way
    
    * added shortcut for _parse_button
    
    * fixing pep8 😅
    
    * extends userge.utils (thks @Krishna-Singhal idea)
    
    Co-authored-by: rking32 <sljohnwick@gmail.com>

--- userge/versions.py
+++ userge/versions.py
@@ -17,8 +17,5 @@ __minor__ = 1
 __micro__ = 7
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
-
-__license__ = ("[GNU GPL v3.0]"
-               "(https://github.com/UsergeTeam/Userge/blob/master/LICENSE)")
-
+__license__ = "[GNU GPL v3.0](https://github.com/UsergeTeam/Userge/blob/master/LICENSE)"
 __copyright__ = "[UsergeTeam](https://github.com/UsergeTeam)"
