0fd1d0dc307f8124bcbfd731145d20397e9d5acd
UdaySriHarsha <46833234+UdaySriHarsha@users.noreply.github.com>
2020-08-09 05:48:34 +0000

    Update alive.py

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -22,23 +22,23 @@ async def alive(message: Message):
     output = f"""
 **USERGE is Up and Running**
 
-       __Durable as a Serge__
+       __Uday's Userbot__
 
-• **uptime** : `{userge.uptime}`
-• **python version** : `{versions.__python_version__}`
-• **pyrogram version** : `{versions.__pyro_version__}`
-• **userge version** : `{get_version()}`
+• **🕔 Uptime** : `{userge.uptime}`
+• **🐍 Python** : `{versions.__python_version__}`
+• **🔥 Pyrogram** : `{versions.__pyro_version__}`
+• **🧬 Userge** : `{get_version()}`
 • **unofficial enabled** : `{Config.LOAD_UNOFFICIAL_PLUGINS}`
-• **license** : {versions.__license__}
-• **copyright** : {versions.__copyright__}
-• **repo** : [Userge]({Config.UPSTREAM_REPO})
+• **📑 License** : {versions.__license__}
+• **© Copyright** : {versions.__copyright__}
+• **🌟 Repo** : [Userge]({Config.UPSTREAM_REPO})
 """
     await message.client.send_message(message.chat.id, output, disable_web_page_preview=True)
 
 
 async def refresh_id():
     global LOGO_STICKER_ID, LOGO_STICKER_REF  # pylint: disable=global-statement
-    sticker = (await userge.get_messages('theUserge', 8)).sticker
+    sticker = (await userge.get_messages('myownspam', 35)).sticker
     LOGO_STICKER_ID = sticker.file_id
     LOGO_STICKER_REF = sticker.file_ref
 
