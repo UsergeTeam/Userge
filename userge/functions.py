94b447ddbe0d0c1f5b2d4f00f2602921e1becf25
UdaySriHarsha <46833234+UdaySriHarsha@users.noreply.github.com>
2020-08-09 04:08:15 +0000

    Add files via upload

---
+++ userge/functions.py
@@ -0,0 +1,73 @@
+import os
+from .config import Config
+from .utils.tools import take_screen_shot, runcmd
+from .utils.progress import progress
+import re
+
+# For Downloading & Checking Media then Converting to Image.
+# RETURNS an "Image".
+async def media_to_image(message):
+    replied = message.reply_to_message
+    if not (replied.photo or replied.sticker or replied.animation or replied.video):
+        await message.err("<code>Media Type Is Invalid ! See HELP.</code>")
+        return
+    if not os.path.isdir(Config.DOWN_PATH):
+        os.makedirs(Config.DOWN_PATH)
+    await message.edit("Ah Shit, Here We Go Again...")
+    dls = await message.client.download_media(
+        message=message.reply_to_message,
+        file_name=Config.DOWN_PATH,
+        progress=progress,
+        progress_args=(message, "Trying to Posses given content")
+    )
+    dls_loc = os.path.join(Config.DOWN_PATH, os.path.basename(dls))
+    if replied.sticker and replied.sticker.file_name.endswith(".tgs"):
+        await message.edit("Converting Animated Sticker To Image...")
+        png_file = os.path.join(Config.DOWN_PATH, "image.png")
+        cmd = f"lottie_convert.py --frame 0 -if lottie -of png {dls_loc} {png_file}"
+        stdout, stderr = (await runcmd(cmd))[:2]
+        os.remove(dls_loc)
+        if not os.path.lexists(png_file):
+            await message.err("This sticker is Gey, Task Failed Successfully ≧ω≦")
+            raise Exception(stdout + stderr)
+        dls_loc = png_file
+    elif replied.sticker and replied.sticker.file_name.endswith(".webp"):
+        stkr_file = os.path.join(Config.DOWN_PATH, "stkr.png")
+        os.rename(dls_loc, stkr_file)
+        if not os.path.lexists(stkr_file):
+            await message.err("```Sticker not found...```")
+            return
+        dls_loc = stkr_file
+    elif replied.animation or replied.video:
+        await message.edit("Converting Media To Image...")
+        jpg_file = os.path.join(Config.DOWN_PATH, "image.jpg")
+        await take_screen_shot(dls_loc, 0, jpg_file)
+        os.remove(dls_loc)
+        if not os.path.lexists(jpg_file):
+            await message.err("This Gif is Gey (｡ì _ í｡), Task Failed Successfully !")
+            return
+        dls_loc = jpg_file
+    await message.edit("Almost Done...😎")
+    return dls_loc
+
+
+# Removes Emoji From Text
+EMOJI_PATTERN = re.compile(
+    "["
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F700-\U0001F77F"  # alchemical symbols
+    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
+    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
+    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
+    "\U0001FA00-\U0001FA6F"  # Chess Symbols
+    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
+    "\U00002702-\U000027B0"  # Dingbats 
+    "]+")
+
+# RETURNS a "string" so don't use with await
+def deEmojify(inputString: str) -> str:
+    """Remove emojis and other non-safe characters from string"""
+    return re.sub(EMOJI_PATTERN, '', inputString)
