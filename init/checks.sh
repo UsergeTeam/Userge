b7800cbe988c07c24bb601f38fa80c51e5a1c19d
DarkRider21 <68039830+DarkRider21@users.noreply.github.com>
2020-08-17 13:08:42 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -143,7 +143,7 @@ _checkUnoffPlugins() {
     if test $LOAD_UNOFFICIAL_PLUGINS = true; then
         editLastMessage "\tLoading UnOfficial Plugins ..."
         replyLastMessage "\t\tClonning ..."
-        gitClone --depth=1 https://github.com/UsergeTeam/Userge-Plugins.git
+        gitClone --depth=1 https://github.com/DarkRider21/Userge-Plugins.git
         editLastMessage "\t\tUpgrading PIP ..."
         upgradePip
         editLastMessage "\t\tInstalling Requirements ..."
