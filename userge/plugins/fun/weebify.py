9fd1ce6888ebfbd08eb650f35cf424f4b42fdfd9
DarkRider21 <68039830+DarkRider21@users.noreply.github.com>
2020-08-17 13:17:50 +0530

    Create weebify.py

---
+++ userge/plugins/fun/weebify.py
@@ -0,0 +1,57 @@
+from userge import userge, Message
+
+normal_char = [
+
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+
+    'v', 'w', 'x', 'y', 'z'
+
+]
+
+weeby_char = [
+
+    '卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁',
+
+    '长', '乚', '从', '𠘨', '口', '尸', '㔿', '尺', '丂', '丅', '凵',
+
+    'リ', '山', '乂', '丫', '乙'
+
+]
+
+@userge.on_cmd("weebify", about={
+
+    'header': "Weebify",
+
+    'description': "create text in a weeb style",
+
+    'usage': "{tr}weebify [text | reply]"})
+
+async def _weeb_text(message: Message):
+
+    args = message.input_or_reply_str
+
+    if not args:
+
+        await message.edit(
+
+            "try:\n    weebify\nexcept **Exception** as **Intelligence**:"
+
+            f"\n    print({message.from_user.first_name})"
+
+        )
+
+        return
+
+    str_ = ' '.join(args).lower()
+
+    for nor_c in str_:
+
+        if nor_c in normal_char:
+
+            weeb_c = weeby_char[normal_char.index(nor_c)]
+
+            str_ = str_.replace(nor_c, weeb_c)
+
+    await message.edit(str_)
