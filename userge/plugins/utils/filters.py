3171c10a36a0eaa34de41fc7bfe4f64b187d7f6f
rking32 <sljohnwick@gmail.com>
2020-08-26 19:57:32 +0530

    optimize filters supported types

--- userge/plugins/utils/filters.py
+++ userge/plugins/utils/filters.py
@@ -20,12 +20,12 @@ FILTERS_DATA: Dict[int, Dict[str, int]] = {}
 FILTERS_CHATS = Filters.create(lambda _, query: query.chat.id in FILTERS_DATA)
 
 _SUPPORTED_TYPES = (":audio:", ":video:", ":photo:", ":document:",
-                    ":sticker:", ":animation:", ":game:", ":voice:",
-                    ":video_note:", ":media:", ":contact:", ":media_group:",
-                    ":location:", ":venue:", ":web_page:", ":poll:",
-                    ":game_high_score:", ":via_bot:", ":service:",
-                    ":mentioned:", ":edited:", ":new_chat_title:",
-                    ":new_chat_photo:", ":delete_chat_photo:", ":pinned_message:")
+                    ":sticker:", ":animation:", ":voice:", ":video_note:",
+                    ":media:", ":game:", ":contact:", ":location:",
+                    ":venue:", ":web_page:", ":poll:", ":via_bot:",
+                    ":forward_date:", ":mentioned:", ":service:",
+                    ":media_group_id:", ":game_high_score:", ":pinned_message:",
+                    ":new_chat_title:", ":new_chat_photo:", ":delete_chat_photo:")
 
 
 def _filter_updater(chat_id: int, name: str, message_id: int) -> None:
