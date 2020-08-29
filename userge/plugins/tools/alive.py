f5526a8190bf77faded21fa2df16df298d66fcbd
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-08-29 19:21:48 +0530

    new template

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -12,69 +12,57 @@ from pyrogram.errors.exceptions.bad_request_400 import BadRequest, ChannelInvali
 from userge.core.ext import RawClient
 from userge import userge, Message, Config, versions, get_version
 
-LOGO_STICKER_ID, LOGO_STICKER_REF = None, None
+LOGO_ID, LOGO_REF = None, None
 
 
 @userge.on_cmd("alive", about={
     'header': "This command is just for fun"}, allow_channels=False)
 async def alive(message: Message):
     await message.delete()
-    await sendit(message)
-    output = f"""
-**uptime** : `{userge.uptime}`
-**version** : `{get_version()}`
-
-• **sudo** : `{_parse_arg(Config.SUDO_ENABLED)}`
-• **anti-spam** : `{_parse_arg(Config.ANTISPAM_SENTRY)}`
-• **dual-mode** : `{_parse_arg(RawClient.DUAL_MODE)}`
-"""
-    if Config.HEROKU_APP:
-        output += f"• **dyno-saver** : `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
-    output += f"""
-• **unofficial** : `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
-
-    **__python__** : `{versions.__python_version__}`
-    **__pyrogram__** : `{versions.__pyro_version__}`
-
-**{versions.__license__}** | **{versions.__copyright__}** | **[Repo]({Config.UPSTREAM_REPO})**
-"""
-    await message.client.send_message(message.chat.id, output, disable_web_page_preview=True)
+    try:
+        await sendit(message.chat.id)
+    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+        await refresh_id()
+        await sendit(message.chat.id)
 
 
 def _parse_arg(arg: bool) -> str:
-    return "✅" if arg else "❎"
+    return "✓" if arg else "X"
 
 
 async def refresh_id():
-    global LOGO_STICKER_ID, LOGO_STICKER_REF  # pylint: disable=global-statement
-    sticker = (await userge.get_messages('theUserge', 8)).sticker
-    LOGO_STICKER_ID = sticker.file_id
-    LOGO_STICKER_REF = sticker.file_ref
-
-
-async def send_sticker(message):
-    try:
-        await message.client.send_sticker(
-            message.chat.id, LOGO_STICKER_ID, file_ref=LOGO_STICKER_REF)
-    except MediaEmpty:
-        pass
+    global LOGO_ID, LOGO_REF  # pylint: disable=global-statement
+    gif = (await userge.get_messages('UserGeOt', 511623)).animation
+    LOGO_ID = gif.file_id
+    LOGO_REF = gif.file_ref
 
 
 async def sendit(message):
-    if LOGO_STICKER_ID:
-        try:
-            await send_sticker(message)
-        except (FileIdInvalid, FileReferenceEmpty, BadRequest):
-            try:
-                await refresh_id()
-            except ChannelInvalid:
-                pass
-            else:
-                await send_sticker(message)
-    else:
+    if not LOGO_ID:
         try:
             await refresh_id()
         except ChannelInvalid:
             pass
-        else:
-            await send_sticker(message)
+    output = f"""
+**⌚️ Uptime** : `{userge.uptime}`
+**💥 Version** : `{get_version()}`
+
+• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
+• **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`
+• **Dual-Mode**: `{_parse_arg(RawClient.DUAL_MODE)}`
+"""
+    if Config.HEROKU_APP:
+        output += f"• **Dyno-Saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
+    output += f"""
+• **unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
+
+    **__Python__**: `{versions.__python_version__}`
+    **__Pyrogram__**: `{versions.__pyro_version__}`
+
+**{versions.__license__}** | **{versions.__copyright__}** | **[Repo]({Config.UPSTREAM_REPO})**
+"""
+    try:
+        await message.reply_animation(
+            LOGO_ID, file_ref=LOGO_REF, caption=output)
+    except MediaEmpty:
+        pass
