c12bbcc59aef576749f953c3b8999ddeb0e63dc5
Himanshu Sharma <65869697+himshk@users.noreply.github.com>
2020-10-08 10:54:14 +0530

    Create die.py

---
+++ userge/plugins/fun/die.py
@@ -0,0 +1,28 @@
+import os
+
+import time
+
+import asyncio
+
+from re import sub
+
+from collections import deque
+
+from random import choice, getrandbits, randint
+
+import wget
+
+import requests
+
+from cowpy import cow
+
+from userge import userge, Message
+
+
+@userge.on_cmd("die$", about={'header': "doob maro"})
+
+async def doobmaro_(message: Message):
+
+    """doobmaro_"""
+
+    await check_and_send(message, "Doob Maro,plox")
