81996d9991bc6b4bfd20da07fbc98461ddb7810f
bhoomi-s-007 <71869549+bhoomi-s-007@users.noreply.github.com>
2020-10-10 11:58:52 +0530

    Update purge.py

--- userge/plugins/admin/purge.py
+++ userge/plugins/admin/purge.py
@@ -19,10 +19,10 @@ from userge import userge, Message
     'usage': "reply {tr}purge to the start message to purge.\n"
              "use {tr}purge [user_id | user_name] to purge messages from that user or use flags",
     'examples': ['{tr}purge', '{tr}purge -u', '{tr}purge [user_id | user_name]']},
-    allow_bots=False, allow_private=False, del_pre=True)
+    allow_bots=False, del_pre=True)
 async def purge_(message: Message):
     await message.edit("`purging ...`")
-    from_user_id = 0
+    from_user_id = None
     if message.filtered_input_str:
         from_user_id = (await message.client.get_users(message.filtered_input_str)).id
     start_message = 0
@@ -39,27 +39,28 @@ async def purge_(message: Message):
         await message.err("invalid start message!")
         return
     start_t = datetime.now()
-    message_ids = range(start_message, message.message_id)
-    list_of_messages = await message.client.get_messages(chat_id=message.chat.id,
-                                                         message_ids=message_ids,
-                                                         replies=0)
-    list_of_messages_to_delete = []
+    # message_ids = range(start_message, message.message_id)
+    list_of_messages = []
     purged_messages_count = 0
-    for a_message in list_of_messages:
-        if len(list_of_messages_to_delete) == 100:
-            await message.client.delete_messages(chat_id=message.chat.id,
-                                                 message_ids=list_of_messages_to_delete)
-            purged_messages_count += len(list_of_messages_to_delete)
-            list_of_messages_to_delete.clear()
-        if from_user_id:
-            if a_message.from_user and from_user_id == a_message.from_user.id:
-                list_of_messages_to_delete.append(a_message.message_id)
-        else:
-            list_of_messages_to_delete.append(a_message.message_id)
-    if list_of_messages_to_delete:
+    async for a_message in message.client.iter_history(
+            chat_id=message.chat.id, limit=None,
+            offset_id=start_message, reverse=True):
+        if (from_user_id and a_message and a_message.from_user
+                and a_message.from_user.id == from_user_id):
+            list_of_messages.append(a_message.message_id)
+        if not from_user_id:
+            list_of_messages.append(a_message.message_id)
+        if len(list_of_messages) >= 100:
+            await message.client.delete_messages(
+                chat_id=message.chat.id,
+                message_ids=list_of_messages
+            )
+            purged_messages_count += len(list_of_messages)
+            list_of_messages = []
+    if list_of_messages:
         await message.client.delete_messages(chat_id=message.chat.id,
-                                             message_ids=list_of_messages_to_delete)
-        purged_messages_count += len(list_of_messages_to_delete)
+                                             message_ids=list_of_messages)
+        purged_messages_count += len(list_of_messages)
     end_t = datetime.now()
     time_taken_s = (end_t - start_t).seconds
     out = f"<u>purged</u> {purged_messages_count} messages in {time_taken_s} seconds."
