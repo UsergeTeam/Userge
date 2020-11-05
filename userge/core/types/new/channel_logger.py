ed4a5855cbef656e1b389f226d92316866d31f21
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-11-05 12:35:58 +0530

    fixed typo. (#187)

--- userge/core/types/new/channel_logger.py
+++ userge/core/types/new/channel_logger.py
@@ -134,7 +134,7 @@ class ChannelLogger:
                 pass pyrogram.Message object which want to forward.
 
             caption (`str`, *optional*):
-                Text or Cpation of the message to be sent.
+                Text or Caption of the message to be sent.
 
         Returns:
             message_id on success or None
