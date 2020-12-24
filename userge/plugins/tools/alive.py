5fe1bed15c216a5ade548a1701798a37d8abd8d3
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2020-12-24 17:08:28 +0530

    telegraph link support to alive media (#212)
    * lets try telegraph link
    
    * fix pep8
    
    * fixing deepsource too

--- userge/plugins/tools/alive.py
+++ userge/plugins/tools/alive.py
@@ -8,7 +8,7 @@
 
 import re
 import asyncio
-from typing import Optional
+from typing import Tuple, Optional
 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import (
@@ -17,11 +17,14 @@ from pyrogram.errors import (
 )
 
 from userge.core.ext import RawClient
-from userge import userge, Message, Config, versions, get_version, logging
 from userge.utils import get_file_id_and_ref
+from userge import userge, Message, Config, versions, get_version, logging
 
 _LOG = logging.getLogger(__name__)
+
+_IS_TELEGRAPH = False
 _IS_STICKER = False
+
 _DEFAULT = "https://t.me/theUserge/31"
 _CHAT, _MSG_ID = None, None
 _LOGO_ID, _LOGO_REF = None, None
@@ -30,7 +33,6 @@ _LOGO_ID, _LOGO_REF = None, None
 @userge.on_cmd("alive", about={
     'header': "This command is just for fun"}, allow_channels=False)
 async def alive(message: Message):
-
     if not (_CHAT and _MSG_ID):
         try:
             _set_data()
@@ -38,19 +40,40 @@ async def alive(message: Message):
             _LOG.exception("There was some problem while setting Media Data. "
                            f"trying again... ERROR:: {set_err} ::")
             _set_data(True)
+
+    alive_text, markup = _get_alive_text_and_markup(message)
+    if _MSG_ID == "text_format":
+        return await message.edit(alive_text, disable_web_page_preview=True, reply_markup=markup)
+    await message.delete()
+    try:
+        await _send_alive(message, alive_text, markup)
+    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+        await _refresh_id(message)
+        await _send_alive(message, alive_text, markup)
+
+
+def _get_mode() -> str:
+    if RawClient.DUAL_MODE:
+        return "Dual"
+    if Config.BOT_TOKEN:
+        return "Bot"
+    return "User"
+
+
+def _get_alive_text_and_markup(message: Message) -> Tuple[str, Optional[InlineKeyboardMarkup]]:
     markup = None
     output = f"""
-**⏱ uptime** : `{userge.uptime}`
-**💡 version** : `{get_version()}`
-**⚙️ mode** : `{_get_mode().upper()}`
+**⏱ Uptime** : `{userge.uptime}`
+**💡 Version** : `{get_version()}`
+**⚙️ Mode** : `{_get_mode().upper()}`
 
-• **sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
-• **pm-guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`
-• **anti-spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`"""
+• **Sudo**: `{_parse_arg(Config.SUDO_ENABLED)}`
+• **Pm-Guard**: `{_parse_arg(not Config.ALLOW_ALL_PMS)}`
+• **Anti-Spam**: `{_parse_arg(Config.ANTISPAM_SENTRY)}`"""
     if Config.HEROKU_APP:
-        output += f"\n• **dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
+        output += f"\n• **Dyno-saver**: `{_parse_arg(Config.RUN_DYNO_SAVER)}`"
     output += f"""
-• **unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
+• **Unofficial**: `{_parse_arg(Config.LOAD_UNOFFICIAL_PLUGINS)}`
 
     **__Python__**: `{versions.__python_version__}`
     **__Pyrogram__**: `{versions.__pyro_version__}`"""
@@ -67,22 +90,7 @@ async def alive(message: Message):
             ],
             [InlineKeyboardButton(text="🎖 GNU GPL v3.0", url=copy_)]
         ])
-    if _MSG_ID == "text_format":
-        return await message.edit(output, reply_markup=markup, disable_web_page_preview=True)
-    await message.delete()
-    try:
-        await _send_alive(message, output, markup)
-    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
-        await _refresh_id(message)
-        await _send_alive(message, output, markup)
-
-
-def _get_mode() -> str:
-    if RawClient.DUAL_MODE:
-        return "dual"
-    if Config.BOT_TOKEN:
-        return "bot"
-    return "user"
+    return (output, markup)
 
 
 def _parse_arg(arg: bool) -> str:
@@ -96,28 +104,42 @@ async def _send_alive(message: Message,
     if not (_LOGO_ID and _LOGO_REF):
         await _refresh_id(message)
     should_mark = None if _IS_STICKER else reply_markup
-    try:
-        await message.client.send_cached_media(chat_id=message.chat.id,
-                                               file_id=_LOGO_ID,
-                                               file_ref=_LOGO_REF,
+    if _IS_TELEGRAPH:
+        try:
+            await message.client.send_document(chat_id=message.chat.id,
+                                               document=Config.ALIVE_MEDIA,
                                                caption=text,
                                                reply_markup=should_mark)
-        if _IS_STICKER:
-            raise ChatSendMediaForbidden
-    except SlowmodeWait as s_m:
-        await asyncio.sleep(s_m.x)
-        text = f'<b>{str(s_m).replace(" is ", " was ")}</b>\n\n{text}'
-        return await _send_alive(message, text, reply_markup)
-    except MediaEmpty:
-        if recurs_count >= 2:
-            raise ChatSendMediaForbidden
-        await _refresh_id(message)
-        return await _send_alive(message, text, reply_markup, recurs_count + 1)
-    except (ChatSendMediaForbidden, Forbidden):
-        await message.client.send_message(chat_id=message.chat.id,
-                                          text=text,
-                                          reply_markup=reply_markup,
-                                          disable_web_page_preview=True)
+        except SlowmodeWait as s_m:
+            await asyncio.sleep(s_m.x)
+            text = f'<b>{str(s_m).replace(" is ", " was ")}</b>\n\n{text}'
+            return await _send_alive(message, text, reply_markup)
+        except Exception:
+            _LOG.error('Telegraph Link Invalid')
+            _set_data(True)
+    else:
+        try:
+            await message.client.send_cached_media(chat_id=message.chat.id,
+                                                   file_id=_LOGO_ID,
+                                                   file_ref=_LOGO_REF,
+                                                   caption=text,
+                                                   reply_markup=should_mark)
+            if _IS_STICKER:
+                raise ChatSendMediaForbidden
+        except SlowmodeWait as s_m:
+            await asyncio.sleep(s_m.x)
+            text = f'<b>{str(s_m).replace(" is ", " was ")}</b>\n\n{text}'
+            return await _send_alive(message, text, reply_markup)
+        except MediaEmpty:
+            if recurs_count >= 2:
+                raise ChatSendMediaForbidden
+            await _refresh_id(message)
+            return await _send_alive(message, text, reply_markup, recurs_count + 1)
+        except (ChatSendMediaForbidden, Forbidden):
+            await message.client.send_message(chat_id=message.chat.id,
+                                              text=text,
+                                              disable_web_page_preview=True,
+                                              reply_markup=should_mark)
 
 
 async def _refresh_id(message: Message) -> None:
@@ -134,27 +156,31 @@ async def _refresh_id(message: Message) -> None:
 
 
 def _set_data(errored: bool = False) -> None:
-    global _CHAT, _MSG_ID, _DEFAULT  # pylint: disable=global-statement
+    global _CHAT, _MSG_ID, _IS_TELEGRAPH  # pylint: disable=global-statement
 
-    pattern = r"^(http(?:s?):\/\/)?(www\.)?(t.me)(\/c\/(\d+)|:?\/(\w+))?\/(\d+)$"
+    pattern_1 = r"^(http(?:s?):\/\/)?(www\.)?(t.me)(\/c\/(\d+)|:?\/(\w+))?\/(\d+)$"
+    pattern_2 = r"^(http(?:s?):\/\/)?(www\.)?telegra\.ph\/([A-Za-z0-9\-]*)$"
     if Config.ALIVE_MEDIA and not errored:
         if Config.ALIVE_MEDIA.lower().strip() == "nothing":
             _CHAT = "text_format"
             _MSG_ID = "text_format"
             return
         media_link = Config.ALIVE_MEDIA
-        match = re.search(pattern, media_link)
-        if match:
-            _MSG_ID = int(match.group(7))
-            if match.group(5):
-                _CHAT = int("-100" + match.group(5))
-            elif match.group(6):
-                _CHAT = match.group(6)
+        match_1 = re.search(pattern_1, media_link)
+        match_2 = re.search(pattern_2, media_link)
+        if match_1:
+            _MSG_ID = int(match_1.group(7))
+            if match_1.group(5):
+                _CHAT = int("-100" + match_1.group(5))
+            elif match_1.group(6):
+                _CHAT = match_1.group(6)
+        elif match_2:
+            _IS_TELEGRAPH = True
         elif "|" in Config.ALIVE_MEDIA:
             _CHAT, _MSG_ID = Config.ALIVE_MEDIA.split("|", maxsplit=1)
             _CHAT = _CHAT.strip()
             _MSG_ID = int(_MSG_ID.strip())
     else:
-        match = re.search(pattern, _DEFAULT)
+        match = re.search(pattern_1, _DEFAULT)
         _CHAT = match.group(6)
         _MSG_ID = int(match.group(7))
