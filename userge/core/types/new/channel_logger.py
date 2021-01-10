f1176f45eeaef4f09c30ddb69b04f2d008b779be
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-11 01:55:05 +0530

    fixed copy_message(s)

--- userge/core/types/new/channel_logger.py
+++ userge/core/types/new/channel_logger.py
@@ -107,9 +107,9 @@ class ChannelLogger:
                 try:
                     # pylint: disable=protected-access
                     if as_copy:
-                        await message._client.copy_message(chat_id=self._id,
-                                                           from_chat_id=message.chat.id,
-                                                           message_id=message.message_id)
+                        await message._client.copy_messages(chat_id=self._id,
+                                                            from_chat_id=message.chat.id,
+                                                            message_id=message.message_id)
                     else:
                         await message._client.forward_messages(chat_id=self._id,
                                                                from_chat_id=message.chat.id,
