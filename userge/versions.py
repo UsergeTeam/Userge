18df0805e0a82b2e4c58f2535e67ef5c0478cbcc
rking32 <sljohnwick@gmail.com>
2021-01-16 21:54:17 +0530

    Update Userge to v0.3.1

--- userge/versions.py
+++ userge/versions.py
@@ -14,7 +14,7 @@ from pyrogram import __version__ as __pyro_version__  # noqa
 
 __major__ = 0
 __minor__ = 3
-__micro__ = 0
+__micro__ = 1
 
 __python_version__ = f"{version_info[0]}.{version_info[1]}.{version_info[2]}"
 __license__ = "[GNU GPL v3.0](https://github.com/UsergeTeam/Userge/blob/master/LICENSE)"
