c7d4caf887eaa8bab024a253913ef1852c101d21
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-28 17:24:34 +0530

    fixed forwarded message command execution

--- userge/core/types/raw/command.py
+++ userge/core/types/raw/command.py
@@ -52,6 +52,8 @@ class Command(Filter):
             outgoing_flt = filters.create(
                 lambda _, __, m:
                 m.via_bot is None
+                and not m.scheduled
+                and not bool(m.forward_from or m.forward_sender_name)
                 and not (m.from_user and m.from_user.is_bot)
                 and (m.outgoing or (m.from_user and m.from_user.is_self))
                 and not (m.chat and m.chat.type == "channel" and m.edit_date)
