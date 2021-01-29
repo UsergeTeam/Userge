7736a1081f5775aba9fbbce54f773525f014f625
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-01-29 14:17:47 +0530

    disabling forward command execution in outgoing too + attempt to fix pep8 lint

--- userge/core/types/raw/command.py
+++ userge/core/types/raw/command.py
@@ -51,23 +51,32 @@ class Command(Filter):
         if filter_me:
             outgoing_flt = filters.create(
                 lambda _, __, m:
-                m.via_bot is None
-                and not m.scheduled
-                and not bool(m.forward_from or m.forward_sender_name)
-                and not (m.from_user and m.from_user.is_bot)
-                and (m.outgoing or (m.from_user and m.from_user.is_self))
-                and not (m.chat and m.chat.type == "channel" and m.edit_date)
-                and (m.text and m.text.startswith(trigger) if trigger else True))
+                m.via_bot is None and not m.scheduled and not bool(
+                    m.forward_from or m.forward_sender_name
+                ) and not (
+                    m.from_user and m.from_user.is_bot
+                ) and (
+                    m.outgoing or (m.from_user and m.from_user.is_self)
+                ) and not (
+                    m.chat and m.chat.type == "channel" and m.edit_date
+                ) and (
+                    m.text and m.text.startswith(trigger) if trigger else True
+                )
+            )
             incoming_flt = filters.create(
                 lambda _, __, m:
-                m.via_bot is None
-                and not m.outgoing
-                and trigger
-                and m.from_user and m.text
-                and ((m.from_user.id in Config.OWNER_ID)
-                     or (Config.SUDO_ENABLED and (m.from_user.id in Config.SUDO_USERS)
-                         and (cname.lstrip(trigger) in Config.ALLOWED_COMMANDS)))
-                and m.text.startswith(Config.SUDO_TRIGGER))
+                m.via_bot is None and not m.outgoing and trigger and m.from_user and m.text and (
+                    (
+                        m.from_user.id in Config.OWNER_ID
+                    ) or (
+                        Config.SUDO_ENABLED and (
+                            m.from_user.id in Config.SUDO_USERS
+                        ) and (cname.lstrip(trigger) in Config.ALLOWED_COMMANDS)
+                    )
+                ) and m.text.startswith(Config.SUDO_TRIGGER) and not m.scheduled and not bool(
+                    m.forward_from or m.forward_sender_name
+                )
+            )
             filters_ = filters_ & (outgoing_flt | incoming_flt)
         return cls(_format_about(about), trigger, pattern, filters=filters_, name=cname, **kwargs)
 
