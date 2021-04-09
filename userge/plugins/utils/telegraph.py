2eb7940df5712a165bbc0ca356aaf5fa5625d8a8
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-04-09 18:10:17 +0530

    disable_web_page_preview=True

--- userge/plugins/utils/telegraph.py
+++ userge/plugins/utils/telegraph.py
@@ -49,7 +49,8 @@ async def telegraph_(message: Message):
             text = content
             header = "Pasted content by @theuserge"
         t_url = post_to_telegraph(header, text)
-        await message.edit(f"**[Here Your Telegra.ph Link!]({t_url})**")
+        jv_text = f"**[Here Your Telegra.ph Link!]({t_url})**"
+        await message.edit(text=jv_text, disable_web_page_preview=True)
         return
     dl_loc = await message.client.download_media(
         message=message.reply_to_message,
