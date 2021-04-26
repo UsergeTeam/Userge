68c434ba992c31775b39c66118c6526dd4465aa2
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-04-26 20:59:27 +0530

    Pep8😬

--- userge/plugins/tools/executor.py
+++ userge/plugins/tools/executor.py
@@ -164,13 +164,13 @@ async def init_func(message: Message):
     rep = message.reply_to_message
     cmd = message.input_str
     if (rep.document and rep.document.file_name.endswith('.txt', '.py')
-        and rep.document.file_size <= 2097152):
+               and rep.document.file_size <= 2097152):
         dl_loc = await message.client.download_media(
-                message=rep,
-                file_name=Config.DOWN_PATH,
-                progress=progress,
-                progress_args=(message, "trying to download")
-            )
+             message=rep,
+             file_name=Config.DOWN_PATH,
+             progress=progress,
+             progress_args=(message, "trying to download")
+        )
         with open(dl_loc, "r") as jv:
             cmd = jv.read()
         os.remove(dl_loc)
