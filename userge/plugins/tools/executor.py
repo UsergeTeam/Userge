2f0e2f5f8a18e0b3f6f70f579dab042d66a0e94f
Jigar varma <71844973+Jigarvarma2005@users.noreply.github.com>
2021-04-26 20:51:27 +0530

    Add support to eval from .py and .txt file😋
    When in telegram we paste code most of times the syntax changes and we need to correct them there, so this

--- userge/plugins/tools/executor.py
+++ userge/plugins/tools/executor.py
@@ -16,10 +16,9 @@ import traceback
 from getpass import getuser
 from os import geteuid
 from types import SimpleNamespace
-
 from userge import userge, Message, Config
-from userge.utils import runcmd
-
+from userge.utils import runcmd, progress
+import os
 CHANNEL = userge.getCLogger()
 
 
@@ -162,7 +161,19 @@ async def term_(message: Message):
 
 
 async def init_func(message: Message):
+    rep = message.reply_to_message
     cmd = message.input_str
+    if (rep.document and rep.document.file_name.endswith('.txt', '.py')
+        and rep.document.file_size <= 2097152):
+        dl_loc = await message.client.download_media(
+                message=rep,
+                file_name=Config.DOWN_PATH,
+                progress=progress,
+                progress_args=(message, "trying to download")
+            )
+        with open(dl_loc, "r") as jv:
+            cmd = jv.read()
+        os.remove(dl_loc)
     if not cmd:
         await message.err("No Command Found!")
         return None
