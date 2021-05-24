4d43ccffb609e7a4ed0caff48d4e4e634d5d2e0b
naninaveen <30980149+naninaveen@users.noreply.github.com>
2021-05-24 18:22:55 +0530

    fixed deepsource errors

--- userge/plugins/utils/direct_links.py
+++ userge/plugins/utils/direct_links.py
@@ -280,7 +280,7 @@ def androidfilehost(url: str) -> str:
 
 
 def onedrive(link: str) -> str:
-    link_without_query = urlparse(link)._replace(query = None).geturl()
+    link_without_query = urlparse(link)._replace(query=None).geturl()
     direct_link_encoded = str(standard_b64encode(bytes(link_without_query, "utf-8")), "utf-8")
     direct_link1 = f"https://api.onedrive.com/v1.0/shares/u!{direct_link_encoded}/root/content"
     return requests.get(direct_link1).next.url
