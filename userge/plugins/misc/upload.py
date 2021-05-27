905cd94eff0f971c6f32523fc0b40d7eba025765
Harsh <65716674+Harsh-br0@users.noreply.github.com>
2021-05-27 19:46:43 +0530

    Convert a single line to a plane for E128 ツ

--- userge/plugins/misc/upload.py
+++ userge/plugins/misc/upload.py
@@ -128,7 +128,11 @@ async def upload_path(message: Message, path: Path, del_path: bool):
             elif _path.is_dir():
                 path_iterator = _path.iterdir()
                 iterator_len = len(list(path_iterator))
-                for i in sorted(path_iterator, key=lambda a: check_numerical_order(a.name, iterator_len)):
+                for i in sorted(path_iterator,
+                                key=(lambda a:
+                                     check_numerical_order(
+                                                           a.name,
+                                                           iterator_len))):
                     explorer(i)
         explorer(path)
     else:
