76d39bbe8f781ee5499737c3e3e44dbddbbb2571
rking32 <sljohnwick@gmail.com>
2021-08-13 19:02:54 +0530

    bug fixes

--- userge/plugins/utils/voice_call.py
+++ userge/plugins/utils/voice_call.py
@@ -347,6 +347,8 @@ async def force_play_music(msg: Message):
                 return
     elif msg.reply_to_message and msg.reply_to_message.audio:
         replied = msg.reply_to_message
+        if not hasattr(replied, '_client'):
+            replied._client = msg.client  # pylint: disable=protected-access
         QUEUE.insert(0, replied)
     else:
         return await reply_text(msg, "Input not found")
