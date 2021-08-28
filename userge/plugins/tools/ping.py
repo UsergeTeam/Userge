c84e2c194371509c42740963f9e2931f112d522d
DeOn <41499793+D30N@users.noreply.github.com>
2021-08-29 03:17:27 +0530

    Update ping.py

--- userge/plugins/tools/ping.py
+++ userge/plugins/tools/ping.py
@@ -19,4 +19,4 @@ async def pingme(message: Message):
     await message.edit('`Pong!`')
     end = datetime.now()
     m_s = (end - start).microseconds / 1000
-    await message.edit(f"**Pong!**\n`{m_s} ms`")
+    await message.edit(f"**Userbot is online!**\n⏱ `{m_s} ms`")
