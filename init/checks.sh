d4d632626273f1c2afed89d194714afc650b9c89
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2021-09-09 19:24:22 +0530

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -165,7 +165,7 @@ _setupPlugins() {
 }
 
 _checkUnoffPlugins() {
-    _setupPlugins UnOfficial true $LOAD_UNOFFICIAL_PLUGINS https://github.com/UsergeTeam/Userge-Plugins.git
+    _setupPlugins UnOfficial true $LOAD_UNOFFICIAL_PLUGINS https://github.com/lostb053/UsergePlugins.git
 }
 
 _checkCustomPlugins() {
