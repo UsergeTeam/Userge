6507eab5b1c011463644a1044885dd4ebd2a8c9a
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-12-09 14:34:23 +0530

    for https://t.me/usergeot/777441

--- userge/plugins/fun/kang.py
+++ userge/plugins/fun/kang.py
@@ -110,7 +110,7 @@ async def kang_(message: Message):
             exist = await message.client.send(
                 GetStickerSet(
                     stickerset=InputStickerSetShortName(
-                        short_name=packname)))
+                        short_name=packname), hash=0))
         except StickersetInvalid:
             pass
         if exist is not False:
@@ -224,7 +224,7 @@ async def sticker_pack_info_(message: Message):
     get_stickerset = await message.client.send(
         GetStickerSet(
             stickerset=InputStickerSetShortName(
-                short_name=replied.sticker.set_name)))
+                short_name=replied.sticker.set_name), hash=0))
     pack_emojis = []
     for document_sticker in get_stickerset.packs:
         if document_sticker.emoticon not in pack_emojis:
