3a62ebd8aac12ee605a6b1880a036bac14137807
Navaneeth R <93482811+NavaneethRenjith8282@users.noreply.github.com>
2021-12-22 15:30:16 +0530

    Added del command and updated purge.py
    Added del command to purge.py file.
    And, updated to delete_msg.py

--- userge/plugins/admin/delete_msg.py
+++ userge/plugins/admin/delete_msg.py
@@ -72,3 +72,35 @@ async def purge_(message: Message):
     time_taken_s = (end_t - start_t).seconds
     out = f"<u>purged</u> {purged_messages_count} messages in {time_taken_s} seconds."
     await message.edit(out, del_in=3)
+
+
+@userge.on_cmd("del", about={'header': "delete replied message"})
+async def del_msg(message: Message):
+    if userge.dual_mode:
+        u_msg_ids = []
+        b_msg_ids = []
+        o_msg_ids = []
+        for m in filter(lambda _: _, (message, message.reply_to_message)):
+            if m.from_user and m.from_user.id == userge.id:
+                u_msg_ids.append(m.message_id)
+            elif m.from_user and m.from_user.id == userge.bot.id:
+                b_msg_ids.append(m.message_id)
+            else:
+                o_msg_ids.append(m.message_id)
+        if u_msg_ids:
+            await userge.delete_messages(message.chat.id, u_msg_ids)
+        if b_msg_ids:
+            await userge.bot.delete_messages(message.chat.id, b_msg_ids)
+        for o_msg_id in o_msg_ids:
+            try:
+                await userge.delete_messages(message.chat.id, o_msg_id)
+            except MessageDeleteForbidden:
+                try:
+                    await userge.bot.delete_messages(message.chat.id, o_msg_id)
+                except MessageDeleteForbidden:
+                    pass
+    else:
+        await message.delete()
+        replied = message.reply_to_message
+        if replied:
+            await replied.delete()
