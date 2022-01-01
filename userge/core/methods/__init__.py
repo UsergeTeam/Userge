a36b3df9419bde64039b6ea0850b824c993e0edd
Krishna Singhal <ylikehits3@gmail.com>
2022-01-01 16:35:19 +0530

    changes copyright verison

--- userge/core/methods/__init__.py
+++ userge/core/methods/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring
 #
-# Copyright (C) 2020-2021 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
 # This file is part of < https://github.com/UsergeTeam/Userge > project,
 # and is released under the "GNU v3.0 License Agreement".
