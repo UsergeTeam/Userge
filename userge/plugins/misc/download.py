a1f5809828f6bbe19b02022eb27c8080722ca86c
rking32 <sljohnwick@gmail.com>
2022-02-06 17:17:54 +0530

    improve download command with media groups

--- userge/plugins/misc/download.py
+++ userge/plugins/misc/download.py
@@ -18,6 +18,7 @@ from json import dumps
 from urllib.parse import unquote_plus
 
 from pySmartDL import SmartDL
+from pyrogram.types import Message as PyroMessage
 
 from userge import userge, Message, Config
 from userge.utils import progress, humanbytes, extract_entities
@@ -50,21 +51,19 @@ async def down_load_media(message: Message):
         await message.edit(f"Downloaded to `{dl_loc}` in {d_in} seconds")
 
 
-async def handle_download(message: Message, resource: Union[Message, str]) -> Tuple[str, int]:
+async def handle_download(message: Message, resource: Union[Message, str],
+                          from_url: bool = False) -> Tuple[str, int]:
     """ download from resource """
-    if not isinstance(resource, Message):
+    if not isinstance(resource, PyroMessage):
         return await url_download(message, resource)
     if resource.media_group_id:
         resources = await message.client.get_media_group(
-            message.chat.id,
+            resource.chat.id,
             resource.message_id
         )
         dlloc, din = [], 0
         for res in resources:
-            dl_loc, d_in = await tg_download(
-                message,
-                res
-            )
+            dl_loc, d_in = await tg_download(message, res, from_url)
             din += d_in
             dlloc.append(dl_loc)
         return dumps(dlloc), din
@@ -91,7 +90,7 @@ async def url_download(message: Message, url: str) -> Tuple[str, int]:
         if chat_id and msg_id:
             resource = await message.client.get_messages(chat_id, msg_id)
             if resource.media_group_id and not bool(match.group(5)):
-                output = await handle_download(message, resource)
+                output = await handle_download(message, resource, True)
             elif resource.media:
                 output = await tg_download(message, resource, True)
             else:
