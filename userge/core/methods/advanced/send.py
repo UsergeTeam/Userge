124ea49e3d2f97c4d32e1256f923b52ef62e8226
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2022-03-24 12:22:17 +0530

    try to disable line too long warning

--- userge/core/methods/advanced/send.py
+++ userge/core/methods/advanced/send.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring
+# pylint: disable=missing-module-docstring, line-too-long
 #
 # Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
