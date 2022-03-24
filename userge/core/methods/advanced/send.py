76742037ab795d0daa98bc0c0cb798f1c3f39a1f
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2022-03-24 12:23:03 +0530

    Update send.py

--- userge/core/methods/advanced/send.py
+++ userge/core/methods/advanced/send.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-module-docstring, line-too-long
+# pylint: disable=missing-module-docstring,E501
 #
 # Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
 #
