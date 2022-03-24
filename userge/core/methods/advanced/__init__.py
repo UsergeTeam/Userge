4325bfd8233aa0683a107857ca6a9e517182fb39
Krishna Singhal <ylikehits3@gmail.com>
2022-03-24 12:05:08 +0530

    Patch DeleteAccount

--- userge/core/methods/advanced/__init__.py
+++ userge/core/methods/advanced/__init__.py
@@ -8,15 +8,10 @@
 #
 # All rights reserved.
 
-__all__ = ['Methods']
+__all__ = ['Advanced']
 
-from .advanced import Advanced
-from .chats import Chats
-from .decorators import Decorators
-from .messages import Messages
-from .users import Users
-from .utils import Utils
+from .send import Send
 
 
-class Methods(Advanced, Chats, Decorators, Messages, Users, Utils):
-    """ userge.methods """
+class Advanced(Send):
+    """ methods.advanced """
